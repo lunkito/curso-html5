f2bfe69cae2c452c58788f1b011b5aabaef5e8ed
lunkito <pablo.martingu@gmail.com>
2016-10-30 19:08:00 +0000

    Add files via upload

--- js/proyecto.js
+++ js/proyecto.js
@@ -6,7 +6,6 @@ window.addEventListener('DOMContentLoaded', function() {
   var inp = document.getElementById('inp')
 
   document.addEventListener('keydown', function(ev) {
-
     if(act > 13)
       act = 0
 
@@ -23,8 +22,46 @@ window.addEventListener('DOMContentLoaded', function() {
       act -= 1
   })
 
-  document.addEventListener('click', function() {
+  document.addEventListener('click', masdos)
+  function masdos() {
     impress.goto(act += 2)
-    console.log(act)
+  }
+
+  // CountUp
+  var options = {
+    useEasing : true,
+    useGrouping : true,
+    separator : ',',
+    decimal : '.',
+    prefix : '',
+    suffix : ''
+  };
+  var cuenta = new CountUp("numero", 0, 10, 0, 6, options)
+  $('#numero').hover(function() {
+    cuenta.start()
   })
+
+  /*Accordion*/
+  $('.accordion').accordion()
+  var clickEv = true
+  if (clickEv) {
+    $('#problit').hover(function() {
+      document.removeEventListener('click', masdos)
+      clickEv != clickEv
+    })
+    // document.removeEventListener('click', function( event ) {
+    //   var target = event.target;
+    //
+    //   // Find closest step element that is not active
+    //   while ( !( target.classList.contains( "step" ) &&
+    //             !target.classList.contains( "active" ) ) &&
+    //             ( target !== document.documentElement ) ) {
+    //       target = target.parentNode;
+    //   }
+    //
+    //   if ( api.goto( target ) ) {
+    //       event.preventDefault();
+    //   }
+    // })
+  }
 })
